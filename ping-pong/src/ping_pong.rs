418b5248098888ee299c9f3c1e629bebeebb21ee
dorin.iancu <dorin.iancu@elrond.com>
2021-08-30 11:28:51 +0300

    add ESDT support + modernize and fix tests

--- ping-pong/src/ping_pong.rs
+++ ping-pong/src/ping_pong.rs
@@ -25,59 +25,73 @@ const PONG_ALL_LOW_GAS_LIMIT: u64 = 3_000_000;
 #[elrond_wasm::contract]
 pub trait PingPong {
     /// Necessary configuration when deploying:
-    /// `ping_amount` - the exact EGLD amounf that needs to be sent when `ping`-ing.
+    /// `token_id` - the Token Identifier of the token that is going to be used. Pass "EGLD" if you want to use EGLD instead of an ESDT.
+    /// `ping_amount` - the exact amount that needs to be sent when `ping`-ing.
     /// `duration_in_seconds` - how much time (in seconds) until contract expires.
     /// `opt_activation_timestamp` - optionally specify the contract to only actvivate at a later date.
     /// `max_funds` - optional funding cap, no more funds than this can be added to the contract.
     #[init]
     fn init(
         &self,
-        ping_amount: &Self::BigUint,
+        token_id: TokenIdentifier,
+        ping_amount: Self::BigUint,
         duration_in_seconds: u64,
-        opt_activation_timestamp: Option<u64>,
+        #[var_args] opt_activation_timestamp: OptionalArg<u64>,
         #[var_args] max_funds: OptionalArg<Self::BigUint>,
-    ) {
-        self.ping_amount().set(ping_amount);
-        let activation_timestamp =
-            opt_activation_timestamp.unwrap_or_else(|| self.blockchain().get_block_timestamp());
+    ) -> SCResult<()> {
+        require!(
+            token_id.is_egld() || token_id.is_valid_esdt_identifier(),
+            "Invalid token provided"
+        );
+
+        self.ping_amount().set(&ping_amount);
+
+        let activation_timestamp = opt_activation_timestamp
+            .into_option()
+            .unwrap_or_else(|| self.blockchain().get_block_timestamp());
         let deadline = activation_timestamp + duration_in_seconds;
-        self.deadline().set(&deadline);
+
         self.activation_timestamp().set(&activation_timestamp);
+        self.deadline().set(&deadline);
         self.max_funds().set(&max_funds.into_option());
+
+        Ok(())
     }
 
-    /// User sends some EGLD to be locked in the contract for a period of time.
+    /// User sends some tokens to be locked in the contract for a period of time.
     /// Optional `_data` argument is ignored.
-    #[payable("EGLD")]
+    #[payable("*")]
     #[endpoint]
     fn ping(
         &self,
-        #[payment] payment: Self::BigUint,
-        #[var_args] _data: OptionalArg<BoxedBytes>,
+        #[payment_token] payment_token: TokenIdentifier,
+        #[payment_amount] payment_amount: Self::BigUint,
     ) -> SCResult<()> {
         require!(
-            payment == self.ping_amount().get(),
+            payment_token == self.accepted_payment_token_id().get(),
+            "invalid payment token"
+        );
+        require!(
+            payment_amount == self.ping_amount().get(),
             "the payment must match the fixed sum"
         );
 
-        let block_timestamp = self.blockchain().get_block_timestamp();
+        let activation_timestamp = self.activation_timestamp().get();
+        let current_block_timestamp = self.blockchain().get_block_timestamp();
         require!(
-            self.activation_timestamp().get() <= block_timestamp,
+            activation_timestamp <= current_block_timestamp,
             "smart contract not active yet"
         );
 
         require!(
-            block_timestamp < self.deadline().get(),
+            current_block_timestamp < self.deadline().get(),
             "deadline has passed"
         );
 
         if let Some(max_funds) = self.max_funds().get() {
+            let sc_balance_after_transfer = self.blockchain().get_sc_balance(&payment_token, 0);
             require!(
-                &self
-                    .blockchain()
-                    .get_sc_balance(&TokenIdentifier::egld(), 0)
-                    + &payment
-                    <= max_funds,
+                sc_balance_after_transfer <= max_funds,
                 "smart contract full"
             );
         }
@@ -85,17 +99,18 @@ pub trait PingPong {
         let caller = self.blockchain().get_caller();
         let user_id = self.user_mapper().get_or_create_user(&caller);
         let user_status = self.user_status(user_id).get();
+
         match user_status {
             UserStatus::New => {
                 self.user_status(user_id).set(&UserStatus::Registered);
                 Ok(())
-            },
+            }
             UserStatus::Registered => {
                 sc_error!("can only ping once")
-            },
+            }
             UserStatus::Withdrawn => {
                 sc_error!("already withdrawn")
-            },
+            }
         }
     }
 
@@ -104,20 +119,25 @@ pub trait PingPong {
         match user_status {
             UserStatus::New => {
                 sc_error!("can't pong, never pinged")
-            },
+            }
             UserStatus::Registered => {
                 self.user_status(user_id).set(&UserStatus::Withdrawn);
+
                 if let Some(user_address) = self.user_mapper().get_user_address(user_id) {
+                    let token_id = self.accepted_payment_token_id().get();
+                    let amount = self.ping_amount().get();
+
                     self.send()
-                        .direct_egld(&user_address, &self.ping_amount().get(), b"pong");
+                        .direct(&user_address, &token_id, 0, &amount, b"pong");
+                        
                     Ok(())
                 } else {
                     sc_error!("unknown user")
                 }
-            },
+            }
             UserStatus::Withdrawn => {
                 sc_error!("already withdrawn")
-            },
+            }
         }
     }
 
@@ -175,6 +195,10 @@ pub trait PingPong {
     }
 
     // storage
+
+    #[view(getAcceptedPaymentToken)]
+    #[storage_mapper("accepted_payment_token_id")]
+    fn accepted_payment_token_id(&self) -> SingleValueMapper<Self::Storage, TokenIdentifier>;
 
     #[view(getPingAmount)]
     #[storage_mapper("ping_amount")]
